454d96598dafbcc7ce6096eacbaaf914ef6985ed
DanRunfola <geogdan@gmail.com>
2020-04-23 04:00:27 +0000

    2020-04-23 daily commit for repo gbRelease. (1587614423.239749)

--- buildMain.py
+++ buildMain.py
@@ -31,6 +31,15 @@ def geoBoundaries_build(buildID, minor=False):
   exceptions["gbReleaseCandidate_2_0_0"]["URL"].append("http://geo.stp.gov.py/user/dgeec/tables/paraguay_2012_departamentos/public")
   exceptions["gbReleaseCandidate_2_0_0"]["URL"].append("https://www.pdok.nl/nl/producten/pdok-downloads/basis-registratie-kadaster/bestuurlijke-grenzen-actueel")
   exceptions["gbReleaseCandidate_2_0_0"]["URL"].append("https://www.statcan.gc.ca/eng/reference/licence")
+  exceptions["gbReleaseCandidate_3_0_0"] = {}
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"] = []
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("http://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/communes ")
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("http://www.istat.it/it/archivio/124086")
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("https://wambachers-osm.website/boundaries/")
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("https://snig.dgterritorio.gov.pt/rndg/srv/por/catalog.search#/metadata/5e3debf1-6cde-4ff2-8301-a86799231907")
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("http://geo.stp.gov.py/user/dgeec/tables/paraguay_2012_departamentos/public")
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("https://www.pdok.nl/nl/producten/pdok-downloads/basis-registratie-kadaster/bestuurlijke-grenzen-actueel")
+  exceptions["gbReleaseCandidate_3_0_0"]["URL"].append("https://www.statcan.gc.ca/eng/reference/licence")
   
   if(minor == True):
     print("The minor build flag has been set - in this mode, only the build scripts will be re-run, using the data from the major version.")
@@ -58,4 +67,4 @@ def geoBoundaries_build(buildID, minor=False):
 if __name__ == "__main__": 
     import buildMain
     importlib.reload(buildMain)
-    buildMain.geoBoundaries_build("gbReleaseCandidate_2_0_1", minor=True)
+    buildMain.geoBoundaries_build("gbReleaseCandidate_3_0_0", minor=False)
