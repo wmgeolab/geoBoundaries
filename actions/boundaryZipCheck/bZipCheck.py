cff9ff36390ca710a87695e4a9ab97894f8501cb
DanRunfola <dan@danrunfola.com>
2020-09-22 15:34:55 -0400

    Wd fix2

--- actions/boundaryZipCheck/bZipCheck.py
+++ actions/boundaryZipCheck/bZipCheck.py
@@ -7,7 +7,7 @@ working = os.environ['GITHUB_WORKSPACE']
 #working = "/home/dan/git/gbRelease"
 print("Python WD: " + working)  
 
-changedFiles = os.environ['steps.files.changes.added_modified']
+changedFiles = os.environ['changes']
 
 #c = ("sourceData/VAT_ADM0.zip\n" +
 #    "sourceData/YEM_ADM0.zip")
