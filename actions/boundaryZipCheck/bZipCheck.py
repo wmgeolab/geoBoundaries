fa94cce191f11134fa3cf84ff199eb60219e31ae
DanRunfola <dan@danrunfola.com>
2020-09-22 12:26:41 -0400

    Update bZipCheck.py

--- actions/boundaryZipCheck/bZipCheck.py
+++ actions/boundaryZipCheck/bZipCheck.py
@@ -1,1 +1,10 @@
-print("Hello Console")
+import os
+
+working = os.environ['GITHUB_WORKSPACE']
+print(os.environ['GITHUB_WORKSPACE'])
+
+      
+with open(working + "/proposedChanges") as f:
+  c = f.read()
+  
+print(c)
