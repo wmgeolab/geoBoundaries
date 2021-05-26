31d5b58365e9c62f42f7e5ae1bb9b41197e81817
DanRunfola <danr@wm.edu>
2021-05-26 11:08:55 -0400

    Moving websites into seperate repositories

--- website/geoDesc/dynamicAssets/regionOptions.js
+++
@@ -1,5 +0,0 @@
-export const regionOptions = [
-{ code:"GLB_ADM0", label: "Global / ADM0 (Countries)"},
-{ code:"RWA_ADM1", label: "Rwanda / ADM1 (nan)"},
-{ code:"RWA_ADM2", label: "Rwanda / ADM2 (nan)"}
-];
